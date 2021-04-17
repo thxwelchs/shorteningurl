e105b1c313ed34cc4a505e42befe4b5529e68d07
thxwelchs <thxwelchs@gmail.com>
2021-04-18 04:20:20 +0900

    chore: init project

---
+++ src/test/java/dev/thxwelchs/shorteningurl/ShorteningurlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thxwelchs.shorteningurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorteningurlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
